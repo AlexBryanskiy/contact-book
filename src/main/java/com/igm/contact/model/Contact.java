a6b2b3d7fa1a7f23ad8b217cfc4b07056ac4e5e2
Alex Bryanskiy <alexandr.bryanskiy@ukr.net>
2020-01-13 11:01:44 +0200

    - add Contact model;

---
+++ src/main/java/com/igm/contact/model/Contact.java
@@ -0,0 +1,92 @@
+package com.igm.contact.model;
+
+import java.util.UUID;
+
+public class Contact {
+
+    private String id;
+    private String firstName;
+    private String lastName;
+    private String fullName;
+    private String phoneNumber;
+    private String cellPhoneNumber;
+    private String address;
+
+    public Contact() {
+        this.id = UUID.randomUUID().toString();
+    };
+
+    public Contact(String firstName, String lastName, String fullName, String phoneNumber, String cellPhoneNumber, String address) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.fullName = fullName;
+        this.phoneNumber = phoneNumber;
+        this.cellPhoneNumber = cellPhoneNumber;
+        this.address = address;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getCellPhoneNumber() {
+        return cellPhoneNumber;
+    }
+
+    public void setCellPhoneNumber(String cellPhoneNumber) {
+        this.cellPhoneNumber = cellPhoneNumber;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    @Override
+    public String toString() {
+        return "Contact{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", fullName='" + fullName + '\'' +
+                ", phoneNumber='" + phoneNumber + '\'' +
+                ", cellPhoneNumber='" + cellPhoneNumber + '\'' +
+                ", address='" + address + '\'' +
+                '}';
+    }
+}
