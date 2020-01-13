8af29035f3b71bc8542eb2b6e23c66da460302f1
Alex Bryanskiy <alexandr.bryanskiy@ukr.net>
2020-01-13 11:11:32 +0200

    - add ContactController;  - add ContactService interface;  - add ContactRepository interface;

---
+++ src/main/java/com/igm/contact/controller/ContactController.java
@@ -0,0 +1,9 @@
+package com.igm.contact.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("api/contact")
+@RestController
+public class ContactController {
+}
